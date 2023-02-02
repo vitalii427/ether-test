a578160223a4e861a0a3be394ec84eeadfd611f4
vitalii427 <vitalii427@yahoo.com>
2023-02-02 11:49:58 +0200

    Few changes

--- script1.js
+++ script1.js
@@ -4,6 +4,7 @@ const {
   getErc20CallDetails,
   getTransactionDetails,
   prepareErc20TranferInputData,
+  getErc20Contract,
 } = require("./lib/ether-test.js");
 
 const InfuraProvider = new ethers.providers.InfuraProvider(
@@ -12,27 +13,47 @@ const InfuraProvider = new ethers.providers.InfuraProvider(
 );
 
 async function Script1() {
+  const transactionHash =
+    "0x2ad2bb00718ab0ed8310dacff9c029ea5d41e038d96c9f52561a1e7948759e99";
+
+  console.log(`Transaction details for ${transactionHash}:`);
   await getTransactionDetails(
     InfuraProvider,
-    "0x2ad2bb00718ab0ed8310dacff9c029ea5d41e038d96c9f52561a1e7948759e99"
+    transactionHash
   );
 
+  console.log(`ERC20 call details for ${transactionHash}:`);
   try {
     await getErc20CallDetails(
       InfuraProvider,
-      "0x2ad2bb00718ab0ed8310dacff9c029ea5d41e038d96c9f52561a1e7948759e99"
+      transactionHash
     );
   } catch (err) {
     console.error("Error: Probably not ERC20 contract");
     console.error(err);
   }
 
-  await prepareErc20TranferInputData(
-    InfuraProvider,
-    "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
-    "0x85b931A32a0725Be14285B66f1a22178c672d69B",
-    "100"
+  // Prepare input data
+  const recipientAddress = "0x85b931A32a0725Be14285B66f1a22178c672d69B";
+  const tokenContractAddress = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
+  const strAmount = "100";
+
+  const contract = getErc20Contract(InfuraProvider, tokenContractAddress);
+  const { symbol, decimals } = await Promise.all([
+    contract.symbol(),
+    contract.decimals(),
+  ]);
+  const data = await prepareErc20TranferInputData(
+    contract,
+    recipientAddress,
+    strAmount,
+    decimals,
   );
+
+  console.log(
+    `Encoded input data to transfer ${strAmount} ${symbol} to ${recipientAddress}:`
+  );
+  console.log(`\t- ${data}`);
 }
 
 Script1();
